d21160a0ca90cece6c629e94847c545a36c4322b
Tarun Gupta <tarungupta_er@rediffmail.com>
2023-02-19 20:54:14 +0530

    Update Restaurant.jsx

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,59 +1,43 @@
-import React,{useEffect,useState} from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import { useLocation } from "react-router";
 import Data from "../Data/Data";
-import RestaurantCard from "../components/RestaurantCard";
-import * as PushAPI from "@pushprotocol/restapi";
-
+import RestaurantCard from "../components/RestaurantCard/RestaurantCard";
+import BookNow from "../components/RestaurantCard/BookNow"
+import Footer from "../components/Footer";
 const Div = styled.div`
-    margin-top: 100px;
+    display: flex;
+    margin-top: 15px;
 `
-// const H1=styled.h1`
-// font-size: 2rem;
-// `
+const Wholediv = styled.div`
+    width: 90%;
+    margin: 150px auto;
+    max-width: 1080px;
+`
 
-
-export default function Restaurant(props) {
-    //SEND NOTIFICATION TO A SINGLE TARGET
-    const sendNotif = async () => {
-    const apiResponse = await PushAPI.payloads.sendNotification({
-      // signer,
-      type: 3, // target
-      identityType: 2, // direct payload
-      notification: {
-        title: "what is title",
-        body: "message from my dapp",
-        // title: `[SDK-TEST] notification TITLE:`,
-        // body: `[sdk-test] notification BODY`
-      },
-      // payload: {
-      //   title: `[sdk-test] payload title`,
-      //   body: `sample msg body`,
-      //   cta: '',
-      //   img: ''
-      // },
-      recipients: "eip155:80001:0x9E9725400681C01e1C1e5678020b3d54D568d842", // recipient address
-      channel: "eip155:80001:0x182a251472D59e0E2942552382b395e99E87AA67", // your channel address
-      env: "staging",
-    });
-    };
+export default function Restaurant() {
     const location = useLocation();
     const path = location.pathname.split('/')[2];
-    const [restraunt,setRestraunt]= useState({});
+    const [Restaurant, setRestaurant] = useState({});
 
     useEffect(() => {
-    const clickedRestrauntData = Data.find(obj => obj._id === path);
-    // console.log(clickedRestrauntData);
-     setRestraunt(clickedRestrauntData);
-    }, [path])
+        const clickedRestaurantData = Data.find(obj => obj._id === path);
+        // console.log(clickedRestaurantData);
+        setRestaurant(clickedRestaurantData);
+    },  [path])
 
-    const XYZ = () => {
-        return(
+    const Restocard = () => {
+        
+        return (
             <Div>
-            <RestaurantCard  name={restraunt.name}/>
-            {/* <button >Place order</button> */}
-            <button onClick={sendNotif}>Place order</button>
+                <RestaurantCard 
+                name={Restaurant.name}
+                rating = {Restaurant.rating}
+                address = {Restaurant.address}
+                cuisine = {Restaurant.type_of_food}
+                />
+                <BookNow />
             </Div>
         )
     }
@@ -61,8 +45,11 @@ export default function Restaurant(props) {
     return (
         <div>
             <Navbar />
-            <XYZ/>
-            
+            <Wholediv>
+                <Restocard />
+            </Wholediv>
+            <Footer/>
+
         </div>
 
     )
