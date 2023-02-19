6a262dcde37a166e6123a09309cd14a66de2341b
Tarun Gupta <tarungupta_er@rediffmail.com>
2023-02-19 21:53:47 +0530

    Update resto.sol

--- contract/resto.sol
+++ contract/resto.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Resto is ERC20 {
+    constructor() ERC20("BFC", "BFC") {  
+    }
+    function mint(address to) public {
+        _mint(to, 10000000000000000000);
+    }
+}
