7035f7302386bc759f71f2c791ad35430061e137
Tarun Gupta <tarungupta_er@rediffmail.com>
2023-02-19 20:56:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
-import React from "react";
-// import Navbar from "./Navbar";
+import React, {useEffect} from "react";
 import Card from "./components/Card"
-import {Route,Routes } from "react-router-dom"
 import Main from "./pages/Main"
-import Rest from "./pages/Restaurant"
 import Restaurant from "./pages/Restaurant";
-
-
+import { Route, Routes, useLocation } from "react-router-dom"
+import {Auth, useAuth} from '@arcana/auth-react'
 
 function App() {
-
-
+  const auth = useAuth();
+  const { pathname, hash, key } = useLocation();
+  useEffect(() => {
+    // if not a hash link, scroll to top
+    if (hash === '') {
+      window.scrollTo(0, 0);
+    }
+    // else scroll to id
+    else {
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 0);
+    }
+  }, [pathname, hash, key]); // do this on route change
+  
+  const onLogin = () => {
+    // authentication page
+  }
+  const account =
+    auth.loading === true
+      ? auth.isLoggedIn === true
+        ? auth.user.address
+        : null
+      : null;
+  console.log("arcana",account)
   return (
     <div>
-
-    <Routes>
+      <div>
+      {auth.loading ? (
+        "Loading"
+      ) : auth.isLoggedIn ? (
+        
+        <Routes>
       <Route exact path="/" element= {<Main/>} />
       <Route path="/wallet" element= {<Card/>} />
-      <Route path="/rest" element= {<Rest/>} />
-      <Route path="/restraunt/:id" element={<Restaurant/>} />
+      <Route path="/restaurant/:id" element={<Restaurant/>} />
     </Routes>
-    {/* <Routes> */}
-      {/* <Route path="/" element={<Card />} /> */}
-      {/* <R  oute path="/payemnt" element={<Card />} /> */}
-
-    {/* </Routes> */}
-   
+      ) : (
+        <div>
+          <Auth externalWallet={true} theme={"light"} onLogin={onLogin}/>
+        </div>
+      )}
+    </div>
+    
     </div>
   );
 }
